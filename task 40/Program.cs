012abd5f528fe095cd2baf7714285eee8c658291
Vadim <vadim-raz@yandex.ru>
2023-05-28 11:00:51 +0300

    задания9

---
+++ task 40/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+// Console.WriteLine("введите 3 числа: ");
+// int i = Convert.ToInt32(Console.ReadLine());
+// int j = Convert.ToInt32(Console.ReadLine());
+// int k = Convert.ToInt32(Console.ReadLine());
+
+// if (i+j>k & j+k>i & i+k>j);
+// Console.WriteLine("Треугольник существует")
+// else 
+// Console.WriteLine("Треугольник не существует");
+
+
+System.Console.WriteLine("Введите сторону A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите сторону B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите сторону C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b + c && b < a+c && c< a + b)
+    System.Console.WriteLine("Треуголник существует");
+else
+    System.Console.WriteLine("Треуголник не существует");
---
+++ task 44/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+System.Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+int num1 = 0;
+int num2 = 1;
+
+System.Console.Write("0 1 ");
+
+for (int i = 2; i < n; i++)
+{
+    res = num1 + num2;
+    num1 = num2;
+    num2 = res;
+    System.Console.Write($"{res} ");
+}
+
